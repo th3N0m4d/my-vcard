d5dc8c89241f929fb20fa8fe5f4593ddbb404d42
th3N0m4d <edielton.estrela.dantas@sap.com>
2026-05-31 19:34:50 +0200

    feat(contact): show toast message after form submission

--- src/js/send-mail.js
+++ src/js/send-mail.js
@@ -1,39 +1,64 @@
 
 $(function() {
-	
+
 	var contactForm = $('#contact-form');
-	
-	contactForm.submit(function()
-	{
-		if(contactForm.valid())
-		{
-			contactForm.find('.submit-area').addClass('loading');
-			var formValues = $(this).serialize();
-			
-			$.post($(this).attr('action'), formValues, function(data){
-				setTimeout(function(){ 
-					hideLoader();
-					contactForm.find('.submit-area').removeClass('loading').addClass("success"); 
-					contactForm.clearForm(); 
-					},1000);
-			});
-		}
-		return false
+
+	contactForm.submit(function(e) {
+		e.preventDefault();
+
+		if (!contactForm.valid()) return;
+
+		var form = contactForm[0];
+		var submitBtn = contactForm.find('.btn-send');
+
+		submitBtn.prop('disabled', true).text('SENDING...');
+
+		fetch(contactForm.attr('action'), {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+			body: new URLSearchParams(new FormData(form)).toString()
+		})
+		.then(function(res) {
+			if (!res.ok) throw new Error('Network response was not ok');
+			contactForm.clearForm();
+			showToast('Message sent! I\'ll get back to you soon.', 'success');
+		})
+		.catch(function() {
+			showToast('Something went wrong. Please try again.', 'error');
+		})
+		.finally(function() {
+			submitBtn.prop('disabled', false).text('SEND');
+		});
 	});
-		
+
 });
 
 
 $.fn.clearForm = function() {
-  return this.each(function() {
-    var type = this.type, tag = this.tagName.toLowerCase();
-    if (tag == 'form')
-      return $(':input',this).clearForm();
-    if (type == 'text' || type == 'password' || tag == 'textarea')
-      this.value = '';
-    else if (type == 'checkbox' || type == 'radio')
-      this.checked = false;
-    else if (tag == 'select')
-      this.selectedIndex = -1;
-  });
+	return this.each(function() {
+		var type = this.type, tag = this.tagName.toLowerCase();
+		if (tag == 'form')
+			return $(':input', this).clearForm();
+		if (type == 'text' || type == 'email' || type == 'password' || tag == 'textarea')
+			this.value = '';
+		else if (type == 'checkbox' || type == 'radio')
+			this.checked = false;
+		else if (tag == 'select')
+			this.selectedIndex = -1;
+	});
 };
+
+
+function showToast(message, type) {
+	var existing = $('#toast');
+	if (existing.length) existing.remove();
+
+	var toast = $('<div id="toast"></div>').text(message).addClass(type);
+	$('body').append(toast);
+
+	setTimeout(function() { toast.addClass('visible'); }, 10);
+	setTimeout(function() {
+		toast.removeClass('visible');
+		setTimeout(function() { toast.remove(); }, 300);
+	}, 4000);
+}
